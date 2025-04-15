8d40f95ed34267f693eed07bc7fff5bf712f2888
adenijimuiz <muizodunayo111@gmail.com>
2025-04-15 13:42:22 +0100

    hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 export const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      
+      <div className="hero">
+        <img src={hero_banner} alt="hero img" className='banner-img' />
+
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p>Discovering his ties to a secret anacient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
